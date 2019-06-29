649a73d69f2cea0a35711e0ab11fa28e77bf760c
Nick <ngagliar@msudenver.edu>
2019-06-29 11:26:42 -0600

    Added Mocking/Unit Testing Tutorial

--- CS_3250_SUM19/Tutorial_Examples/Mocking/test_employee.py
+++ CS_3250_SUM19/Tutorial_Examples/Mocking/test_employee.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import patch
 from employee import Employee
 
 
@@ -30,7 +31,7 @@ class TestEmployee(unittest.TestCase):
         self.emp_2.first = 'Sally'
 
         self.assertEqual(self.emp_1.fullname, 'Peter Smith')
-        self.assertEqual(self.emp_2.fullname, 'Sally')
+        self.assertEqual(self.emp_2.fullname, 'Sally Crawford')
 
 
     def test_apply_raise(self):
@@ -41,8 +42,23 @@ class TestEmployee(unittest.TestCase):
         self.emp_2.apply_raise()
 
         self.assertEqual(self.emp_1.pay, 52500)
-        self.assertEqual(self.self.emp_2.pay, 63000)
+        self.assertEqual(self.emp_2.pay, 63000)
 
+
+    def test_monthly_schedule(self):
+        with patch('employee.requests.get') as mocked_get:
+            mocked_get.return_value.ok = True
+            mocked_get.return_value.text = 'Success!'
+
+            schedule = self.emp_1.monthly_schedule('May')
+            mocked_get.assert_called_with('http://company.com/Smith/May')
+            self.assertEqual(schedule, 'Success!')
+
+            mocked_get.return_value.ok = False 
+
+            schedule = self.emp_2.monthly_schedule('June')
+            mocked_get.assert_called_with('http://company.com/Crawford/June')
+            self.assertEqual(schedule, 'Bad Response!')
 
 if __name__ == '__main__':
     unittest.main() 
